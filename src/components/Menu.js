a098594ad2c3cd7579df7c564c41002f6a064e12
Danitza Bonilla <danitzabonilla@gmail.com>
2020-09-10 17:28:34 -0500

    cambios menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Menu = () => {
   const categories = [
@@ -30,26 +30,37 @@ const Menu = () => {
   ];
 
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light ">
+    <nav className="navbar navbar-expand-lg navbar-light ">
+      <div className="container">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
           <ul className="navbar-nav">
             <li className="nav-item">
-            <NavLink to={`/`} exact className="nav-link" activeClassName='active'>
+              <NavLink to={`/`} exact className="nav-link" activeClassName="active">
                 Home <span className="sr-only">(current)</span>
-                </NavLink>
+              </NavLink>
             </li>
             {categories.map((element, i) => (
               <li className="nav-item" key={i}>
-                <NavLink to={`/category/${element.id}`} className="nav-link" activeClassName='active'>
+                <NavLink to={`/category/${element.id}`} className="nav-link" activeClassName="active">
                   {element.name}
                 </NavLink>
               </li>
             ))}
           </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
